ed8b0db73d4d4802c755d3d6b4610fb610373b26
edoceo <code@edoceo.com>
2013-08-21 08:55:07 -0700

    improve link processor

--- lib/Chat_Line.php
+++ lib/Chat_Line.php
@@ -9,20 +9,45 @@ class Chat_Line
 {
 	private function __construct() { /* No */ }
 
-	public static function format($raw)
+	public static function format($line)
 	{
-		$ret = trim($raw);
-
-		// Backticks become <code>
-		$ret = preg_replace('/\s`(.+?)`\s/',' <code>`$1`</code> ',$ret);
-
-		// * become <strong>
-		$ret = preg_replace('/\s\*(.+?)\*\s/',' <strong>*$1*</strong> ',$ret);
+		$line = trim($line);
 
 		// General Hyperlinks
-		$ret = preg_replace('|(https?://[\w\#\%\+\-\.\/\;\=\?\~]+)|','<a href="$1" target="_blank">$1</a>',$ret);
+		// $ret = preg_replace('|(https?://[^ <]+)\.?|','<a href="$1" target="_blank">$1</a>',$ret);
+		$line = self::_link($line);
 
-		return $ret;
+		// Backticks become <code>
+		$line = preg_replace('/\s`(.+?)`\s/',' <code>`$1`</code> ',$line);
+
+		// * become <strong>
+		$line = preg_replace('/\s\*(.+?)\*\s/',' <strong>*$1*</strong> ',$line);
+
+		return $line;
+	}
+
+	private static function _link($text)
+	{
+		if (preg_match_all('|\b(https?://[^ ]+)|',$text,$m)) {
+			$idx = __LINE__;
+			$set = array();
+			foreach ($m[1] as $uri) {
+				$tok = sprintf('uri%08x',++$idx);
+				$set[$tok] = $uri;
+				$text = str_replace($uri,$tok,$text);
+			}
+			foreach ($set as $tok=>$uri) {
+				// Handle Trailing Puncuation
+				if (preg_match('/([\.\+\'\"])$/',$uri,$m)) {
+					$uri = substr($uri,0,-1);
+					$rep = '<a href="' . $uri . '">' . preg_replace('|^https?://|',null,$uri) . '</a>' . $m[1];
+				} else {
+					$rep = '<a href="' . $uri . '">' . preg_replace('|^https?://|',null,$uri) . '</a>';
+				}
+				$text = str_replace($tok,$rep,$text);
+			}
+		}
+		return $text;
 	}
 
 }
