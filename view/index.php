fbb0b01536c77e754b3b15cfe79f1d9bc6f32a35
edoceo <code@edoceo.com>
2013-08-20 18:57:56 -0700

    The homepage

---
+++ view/index.php
@@ -0,0 +1,39 @@
+<h1>Nuntio</h1>
+
+<h2>Latin: announce, report, relate, herald, give forth, intimate authoritatively</h2>
+<p>We are building a neat-o Team Media Communication platform.</p>
+                             
+<form action="/chat/join" method="post">
+<input name="user" placeholder="username (email)" type="text">
+<input name="room" placeholder="room" type="text">
+<button class="exec" name="a" value="join">Join</button>
+</form>
+
+
+<h2>Alternatives</h2>
+<p>Alternatives to Nuntio:
+<a href="https://campfirenow.com/" rel="nofollow">CampFire Chat</a>,
+<a href="https://www.hipchat.com/" rel="nofollow">HipChat</a>,
+<a href="https://hall.com/" rel="nofollow">Hall</a>,
+<a href="" rel="nofollow">Unison</a>,
+<a href="" rel="nofollow">CoMeeting</a>,
+Yammer,
+S?????,
+<a href="https://www.echowaves.com/" rel="nofollow">Echowaves
+</p>
+
+<h2>Other Remote Communication Tools</h2>
+<h3>Skype &amp;c</h3>
+<p>
+These tools that offer group chat are great, but what about when folks are off-line?
+What about chat history?  What about intergration with my Task tracker, Code manager or Wiki?
+</p>
+
+
+<h2>Task Management Tools</h2>
+<pre>
+* Trac
+* Redmine / ChiliProject
+* Jira
+* Bugzilla
+</pre>
